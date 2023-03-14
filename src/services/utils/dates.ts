aa35332c4ee2dadb0635e234f7bd10000402de99
kiri <kirillknizhov@mail.ru>
2023-03-15 03:13:36 +0700

    dates little update, geojson import little fix

--- src/services/utils/dates.ts
+++ src/services/utils/dates.ts
@@ -1,41 +1,43 @@
 export type DateInterval = [Date, Date];
 export type DatesConfig = { [key: string]: DateInterval[] };
 export const getDateIntervals = (
-  yearsIntervals: [number, number][],
-  monthIntervals: [number, number][],
+  yearsIntervals: ([number, number] | number)[],
+  monthIntervals: ([number, number] | number)[],
   intervalsInMonth?: [number, number | string][]
 ): DateInterval[] =>
-  yearsIntervals.flatMap(([yearStart, yearEnd]) => {
+  yearsIntervals.flatMap((years) => {
+    const [yearStart, yearEnd] =
+      typeof years === "number" ? [years, years] : years;
     const res: [Date, Date][] = [];
     for (let year = yearStart; year <= yearEnd; year++) {
-      const currentYearIntervals = monthIntervals.flatMap(
-        ([monthStart, monthEnd]) => {
-          const monthsRes: [Date, Date][] = [];
-          if (!intervalsInMonth) {
-            res.push([
-              new Date(year, monthStart, 1),
-              new Date(year, monthEnd + 1, 0),
-            ]);
-          } else {
-            for (let month = monthStart; month <= monthEnd; month++) {
-              const currentMonthIntervals = intervalsInMonth.map(
-                ([dateStart, dateEnd]) => {
-                  let start = new Date(year, month, dateStart);
-                  let end = new Date(year, month);
-                  if (typeof dateEnd === "string") {
-                    end = new Date(year, month + 1, 0);
-                  } else {
-                    end.setDate(dateEnd);
-                  }
-                  return [start, end] as [Date, Date];
+      const currentYearIntervals = monthIntervals.flatMap((months) => {
+        const [monthStart, monthEnd] =
+          typeof months === "number" ? [months, months] : months;
+        const monthsRes: [Date, Date][] = [];
+        if (!intervalsInMonth) {
+          res.push([
+            new Date(year, monthStart, 1),
+            new Date(year, monthEnd + 1, 0),
+          ]);
+        } else {
+          for (let month = monthStart; month <= monthEnd; month++) {
+            const currentMonthIntervals = intervalsInMonth.map(
+              ([dateStart, dateEnd]) => {
+                let start = new Date(year, month, dateStart);
+                let end = new Date(year, month);
+                if (typeof dateEnd === "string") {
+                  end = new Date(year, month + 1, 0);
+                } else {
+                  end.setDate(dateEnd);
                 }
-              );
-              monthsRes.push(...currentMonthIntervals);
-            }
+                return [start, end] as [Date, Date];
+              }
+            );
+            monthsRes.push(...currentMonthIntervals);
           }
-          return monthsRes;
         }
-      );
+        return monthsRes;
+      });
       res.push(...currentYearIntervals);
     }
     return res;

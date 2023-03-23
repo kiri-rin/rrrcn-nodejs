63e4c929cc30a394ff100caed265c007dbf2e6d5
kiri <kirillknizhov@mail.ru>
2023-03-24 04:20:00 +0700

    little fix

--- src/for-papers/karatau-old/get-gee-data.ts
+++
@@ -1,7 +0,0 @@
-import { withGEE } from "../../index";
-import { extractData } from "../../controllers/extract-data/extract-data";
-import { karatau_old_data_export_config } from "./configs/ee-data-config";
-
-withGEE(async () => {
-  await extractData(karatau_old_data_export_config);
-});

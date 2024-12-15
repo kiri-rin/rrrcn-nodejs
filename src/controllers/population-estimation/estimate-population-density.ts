ce24b2de752a984629ca6ed8a4152e3ae316c1d4
kirilllknizhov <kirillknizhov@mail.ru>
2024-12-15 20:01:41 +0700

    Added fields to density

--- src/controllers/population-estimation/estimate-population-density.ts
+++ src/controllers/population-estimation/estimate-population-density.ts
@@ -9,6 +9,7 @@ import FormData from "form-data";
 import { parse } from "csv-parse/sync";
 import shp from "shpjs";
 import { JSCSVTable } from "../../utils/points";
+import {stringify} from "csv-stringify/lib/sync";
 const apiRService = axios.create({
   baseURL: process.env.R_BASE_URL || "http://localhost:8000",
 });
@@ -35,9 +36,11 @@ export const estimatePopulationDensity = async (
   const minDensity = averageDensity / (1 + 1.64 * averageDensityError);
   const maxDensity = averageDensity * (1 + 1.64 * averageDensityError);
   const res = {
+    n:totalNumber,
     minDensity,
     maxDensity,
     averageDensity,
+    averageDensityError,
     averageNumber: averageDensity * config.totalArea,
     minNumber: minDensity * config.totalArea,
     maxNumber: maxDensity * config.totalArea,
@@ -46,6 +49,10 @@ export const estimatePopulationDensity = async (
   await writeFile(
     `${config.outputs || ""}/density.json`,
     JSON.stringify(res, null, 4)
+  );
+  await writeFile(
+    `${config.outputs || ""}/density.csv`,
+    getCsv(res)
   );
   console.log({ res });
   return res;
@@ -73,3 +80,35 @@ const calculateError = ({
   }, 0);
   return (1 / density) * Math.sqrt((1 / totalArea) * X);
 };
+export const getCsv=(res:{
+  n:number,
+  minDensity:number,
+  maxDensity:number,
+  averageDensity:number,
+  averageDensityError:number,
+  averageNumber:number,
+  minNumber:number,
+  maxNumber:number,
+})=>{
+const keys=[
+  "minDensity",
+  "maxDensity",
+  "averageDensity",
+  "averageDensityError (SE)",
+  "averageNumber (estimation)",
+  "minNumber",
+  "maxNumber",
+  "n",
+]
+const values=[
+  res.minDensity,
+  res.maxDensity,
+  res.averageDensity,
+  res.averageDensityError,
+  res.averageNumber,
+  res.minNumber,
+  res.maxNumber,
+  res.n,
+]
+  return stringify([keys,values])
+}
